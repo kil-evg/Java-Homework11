f20581b50f4acd767aab0c06ead49e1467e2a25f
EKil <loreleyko@gmail.com>
2024-10-13 14:49:55 +0200

    First commit

--- src/main/java/ait/imagga/dto/ColorsDto.java
+++ src/main/java/ait/imagga/dto/ColorsDto.java
@@ -8,9 +8,6 @@ import java.util.List;
 @Getter
 @ToString
 public class ColorsDto {
-//    private String closest_palette_color;
-//    private String closest_palette_color_parent;
-//    private double percent;
     private List<ColorInfoDto> background_colors;
     private List<ColorInfoDto> foreground_colors;
     private List<ColorInfoDto> image_colors;
--- src/main/java/ait/imagga/dto/ResultDto.java
+++ src/main/java/ait/imagga/dto/ResultDto.java
@@ -4,7 +4,6 @@ import lombok.Getter;
 import lombok.ToString;
 
 @Getter
-@ToString
 public class ResultDto {
     private ColorsDto colors;
 }
